3071d17b2bce84c0bc0674a05e9fa46e5c248f8c
JulyFox <53467350+JulyFox@users.noreply.github.com>
2020-03-12 11:14:44 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -11,8 +11,7 @@ import 'element-ui/lib/theme-chalk/index.css'
 Vue.use(ElementUI)
 
 Vue.config.productionTip = false
-//Vue.prototype.url = 'http://47.96.117.121:5003';
-//axios.defaults.baseURL = 'http://47.96.117.121:5003/api/'
+
 
 
 /* eslint-disable no-new */
